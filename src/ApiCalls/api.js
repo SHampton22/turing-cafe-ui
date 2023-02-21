8344f91e6d6289cb007322b110fbd36cb833365f
Sarah Hampton <stoluba@gmail.com>
2023-02-21 13:45:17 -0500

    Create post in apicalls

--- src/ApiCalls/api.js
+++ src/ApiCalls/api.js
@@ -8,4 +8,20 @@ const fetchReservations = () => {
   })
 }
 
-export {fetchReservations}
+const postReservation = () => {
+  return fetch('http://localhost:3001/api/v1/reservations', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json'
+    }
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('error', response.text)
+      }
+      return response.json()
+    })
+  })
+}
+
+export {fetchReservations, postReservation}
